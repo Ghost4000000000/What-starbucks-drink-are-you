a1668c626ee2e80955ad6d6610c4cec76432883f
Ghost4000000000 <145487623+Ghost4000000000@users.noreply.github.com>
2024-03-07 09:55:33 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+let option1 = document.querySelector(".option-one").value;
+let option2 = document.querySelector(".option-two").value;
+let option3 = document.querySelector(".option-three").value;
+let option4 = document.querySelector(".option-four").value;
+
+let option5 = document.querySelector(".option-five").value;
+let option6 = document.querySelector(".option-six").value;
+let option7 = document.querySelector(".option-seven").value;
+let option8 = document.querySelector(".option-eight").value;
+
+
+let result = document.querySelector(".result");
